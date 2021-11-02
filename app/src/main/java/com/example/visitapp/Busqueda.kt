3a92b482c848f53595c5e6cdc28771cbb993cf6d
Erick Gómez <erick.gomez1003@gmail.com>
2021-11-01 18:02:05 -0600

    Adición de activity_busqueda.xml y activity_seleccion.xml.

---
+++ app/src/main/java/com/example/visitapp/Busqueda.kt
@@ -0,0 +1,11 @@
+package com.example.visitapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Busqueda : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_busqueda)
+    }
+}
