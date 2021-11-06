42895c31e8ab5eec315e485fc7bbeb0465390e8e
Erick Gómez <erick.gomez1003@gmail.com>
2021-11-06 17:13:23 -0600

    Creado el layout y class respectivo para el mensaje de dirigirse a recepción (activity_mensaje.xml y Mensaje.kt).
    Agregada esta nueva activity al manifest.
    
    Agregado el viewBinding al manifest.
    
    Agregados los onClickListener de MainActivity.kt.
    
    Creado el style de la nueva activity_mensaje.xml en themes.xml.

--- app/src/main/java/com/example/visitapp/MainActivity.kt
+++ app/src/main/java/com/example/visitapp/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.visitapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import com.example.visitapp.databinding.ActivityMainBinding
+import android.R
+import android.app.Dialog
+import android.graphics.Color
+import androidx.fragment.app.FragmentTransaction
+import android.graphics.drawable.ColorDrawable
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btDepartment.setOnClickListener{
+            val intent = Intent(this, Busqueda::class.java)
+            startActivity(intent)
+        }
+
+        binding.btPerson.setOnClickListener{
+            val intent = Intent(this, Seleccion::class.java)
+            startActivity(intent)
+        }
+
+        binding.btHelp.setOnClickListener{
+            val intent = Intent(this, Mensaje::class.java)
+            startActivity(intent)
+
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/visitapp/Mensaje.kt
@@ -0,0 +1,21 @@
+package com.example.visitapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.visitapp.databinding.ActivityMainBinding
+import com.example.visitapp.databinding.ActivityMensajeBinding
+
+class Mensaje : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMensajeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMensajeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btConfirm.setOnClickListener{
+            onBackPressed()
+        }
+    }
+}
